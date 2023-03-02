b2e3aa46ec26ff0a54a27bcea040b5a7f008d04c
Diego Dominguez <diegodguez@hotmail.com>
2023-03-02 01:07:18 -0700

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Eco Water',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
