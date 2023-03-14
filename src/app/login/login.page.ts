a8ce7af79b509b309a830036b07999cdf83a896f
Diego-Dominguezz <Diegodguez@hotmail.com>
2023-03-14 11:53:30 -0600

    eliminado navigate de login ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -24,12 +24,6 @@ export class LoginPage implements OnInit {
 
   ngOnInit() {
   }
-  forgotNavigate(){
-    console.log('navigate?')
-    this.router.navigate(['forgotPass']).then(x=>{
-      console.log('navigated',x)
-    })
-  }
   submitForm() {
     this.authService.login(this.form.value);
   }
